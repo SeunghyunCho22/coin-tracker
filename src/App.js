026a0ef6fc8af2be8ec0c49bbe1e91cf83c45bbd
Jake Seunghyun Cho <shn07102@gmail.com>
2024-01-26 22:49:03 +0900

    coin-tracker

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins!</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin,))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
